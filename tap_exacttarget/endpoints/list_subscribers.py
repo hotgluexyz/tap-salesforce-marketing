8bb592c28b81587f68d37625f3aa0efeccb124c8
Key <keynarafaelp@gmail.com>
2026-06-26 09:40:48 -0500

    clean subscriber commented code

--- tap_exacttarget/endpoints/list_subscribers.py
+++ tap_exacttarget/endpoints/list_subscribers.py
@@ -21,19 +21,6 @@ def _get_subscriber_key(list_subscriber):
 
 
 def _get_list_subscriber_filter(_list, start):
-    # return {
-    #     'LogicalOperator': 'AND',
-    #     'LeftOperand': {
-    #         'Property': 'ListID',
-    #         'SimpleOperator': 'equals',
-    #         'Value': _list.get('ID'),
-    #     },
-    #     'RightOperand': {
-    #         'Property': 'ModifiedDate',
-    #         'SimpleOperator': 'greaterThan',
-    #         'Value': start
-    #     }
-    # }
     return {
         'Property': 'ModifiedDate',
         'SimpleOperator': 'greaterThan',
@@ -88,18 +75,8 @@ class ListSubscriberDataAccessObject(DataAccessObject):
         # pass config to return start date if not bookmark is found
         start = get_last_record_value_for_table(self.state, table, self.config)
 
-        # Note1: for incrementals list_subscribers was iterating daily from start_date instead of filtering by greaterthan: start_date commenting in case it's needed lately
-        # pagination_unit = self.config.get(
-        #     'pagination__list_subscriber_interval_unit', 'days')
-        # pagination_quantity = self.config.get(
-        #     'pagination__list_subscriber_interval_quantity', 1)
-
-        # unit = {pagination_unit: int(pagination_quantity)}
-
         all_subscribers_list = self._get_all_subscribers_list()
 
-        # commenting due to Note1:
-        # while before_now(start):
         stream = request('ListSubscriber',
                             FuelSDK.ET_List_Subscriber,
                             self.auth_stub,
@@ -143,6 +120,3 @@ class ListSubscriberDataAccessObject(DataAccessObject):
 
             save_state(self.state)
 
-            # commenting due to Note1
-            # start = end
-            # end = increment_date(start, unit)
